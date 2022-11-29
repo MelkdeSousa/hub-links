274c78a1b1c4437348e7610be2238ed533deec51
Melk de Sousa <melk.sousap@gmail.com>
2022-11-29 18:59:24 -0300

    execute usecase create link

--- src/functions/links/createLink/handler.ts
+++ src/functions/links/createLink/handler.ts
@@ -1,14 +1,24 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import { CreateLinkUseCase } from 'src/core/usecases/CreateLink';
+import { connection } from 'src/infra/database/connection.prisma';
+import { PrismaLinkRepository } from 'src/infra/repositories/LinkRepository.prisma';
 
 import schema from './schema';
 
+const linkRepository = new PrismaLinkRepository(connection)
+const createLinkUseCase = new CreateLinkUseCase(linkRepository)
+
 const createLink: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
-  });
+const { title, url } = event.body
+
+
+  createLinkUseCase.execute({
+    title, url
+  })
+
+  return formatJSONResponse(201);
 };
 
 export const main = middyfy(createLink);
