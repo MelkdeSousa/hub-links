ffe4d372e8a6ae7681dac97feea6e175f393998e
Melk de Sousa <melk.sousap@gmail.com>
2022-11-29 23:41:22 -0300

    try error save link

--- src/core/usecases/CreateLink.ts
+++ src/core/usecases/CreateLink.ts
@@ -1,21 +1,23 @@
-import { ILinkWriteRepository } from "../contracts/LinkRepository"
-import { Link } from "../entities/Link"
+import { ILinkWriteRepository } from '../contracts/LinkRepository';
+import { Link } from '../entities/Link';
 
 export class CreateLinkUseCase {
-    constructor(private _linkWriteRepository: ILinkWriteRepository) { }
+  constructor(private _linkWriteRepository: ILinkWriteRepository) {}
 
-    async execute(input: CreateLinkInput): Promise<void> {
-        const { instance: link, error } = Link.create(input)
+  async execute(input: CreateLinkInput): Promise<void> {
+    const { instance: link, error } = Link.create(input);
 
-        if (error) throw error
+    if (error) throw error;
 
-        this._linkWriteRepository.save(link)
-        
-        console.table(link);
+    try {
+      await this._linkWriteRepository.save(link);
+    } catch (error) {
+      throw new Error('can not save link, raison: ' + error.message);
     }
+  }
 }
 
 export type CreateLinkInput = {
-    title: string
-    url: string
-}
+  title: string;
+  url: string;
+};
