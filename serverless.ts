be4b90540f5af8699dd539703b54d9a937a18c61
Melk de Sousa <melk.sousap@gmail.com>
2022-11-29 22:14:52 -0300

    add prettier

--- serverless.ts
+++ serverless.ts
@@ -17,7 +17,7 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-      DATABASE_URL: '${ssm:/DATABASE_URL_HUB_LINKS}'
+      DATABASE_URL: '${ssm:/DATABASE_URL_HUB_LINKS}',
     },
   },
   // import the function via paths
