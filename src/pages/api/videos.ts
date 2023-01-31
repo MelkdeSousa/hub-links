f3815a0cb0158ee8861c8dceffa7badcf91d5526
Melk de Sousa <melk.sousap@gmail.com>
2023-01-31 10:01:22 -0300

    endpoint to list all videos with nextjs

---
+++ src/pages/api/videos.ts
@@ -0,0 +1,17 @@
+import { listAllVideos } from "@functions/videos";
+import { NextApiHandler } from "next";
+
+import { Video } from '../../core/entities'
+
+export type ResponseVideos = {
+  videos: Video[]
+}
+
+const handler: NextApiHandler<ResponseVideos> = async (_req, res) => {
+  const output = await listAllVideos()
+
+  res.status(200).json({ videos: output })
+}
+
+export default handler
+
