f5c7a7c1833349261f27d3d893f0ff36ec5db141
Melk de Sousa <melk.sousap@gmail.com>
2023-02-01 14:29:16 -0300

    add ant design lib

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,33 +1,7 @@
 import { useEffect, useState } from "react"
 import { Video } from '../core/entities'
 import Player from 'react-player/lazy'
-
-const useGetVideos = () => {
-  const [videos, setVideos] = useState<Video[]>([])
-  const [loading, setLoading] = useState(false)
-
-  const getVideos = async ({ take, skip }: { skip?: number, take?: number }) => {
-    setLoading(true)
-    try {
-      const response = await fetch('/api/videos')
-
-      const data = await response.json()
-
-      setVideos(data.videos)
-    } catch (err) {
-
-    } finally {
-      setLoading(false)
-    }
-
-  }
-
-  return {
-    getVideos,
-    videos,
-    loading
-  }
-}
+import { useGetVideos } from '../hooks'
 
 const HomePage = () => {
   const { loading, getVideos, videos } = useGetVideos()
