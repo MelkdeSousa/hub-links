14dfb593d9fc16b643095875b9b9f8ea20d685b3
Melk de Sousa <melk.sousap@gmail.com>
2023-02-01 14:28:49 -0300

    export useGetVideos hook

---
+++ src/hooks/useGetVideos.ts
@@ -0,0 +1,28 @@
+export const useGetVideos = () => {
+  const [videos, setVideos] = useState<Video[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const getVideos = async ({ take, skip }: { skip?: number, take?: number }) => {
+    setLoading(true)
+    try {
+      const response = await fetch('/api/videos')
+
+      const data = await response.json()
+
+      setVideos(data.videos)
+    } catch (err) {
+
+    } finally {
+      setLoading(false)
+    }
+
+  }
+
+  return {
+    getVideos,
+    videos,
+    loading
+  }
+}
+
+
