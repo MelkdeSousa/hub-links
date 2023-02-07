373de9225b1ea190ca1ecce8c8bff911c416ef23
Melk de Sousa <melk.sousap@gmail.com>
2023-02-07 12:25:14 -0300

    useMedia hook to items by row

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,19 @@
 import { Input, Layout } from 'antd';
 import { CardVideo, Grid } from '../components';
+import { useMedia } from '../hooks';
 
 import useSWR from 'swr';
 import { fetcher } from '../infra/http/fetcher';
 
 const HomePage = () => {
   const { data } = useSWR('/api/videos', fetcher);
+
+  const columnCount = useMedia<number>(
+    // Media queries
+    ['(min-width: 1000px)', '(min-width: 600px)', '(max-width: 600px)'],
+    [4, 2, 1],
+    4,
+  );
 
   const onSearch = (value: string) => console.log(value);
 
@@ -26,7 +34,7 @@ const HomePage = () => {
           <Grid
             data={data.videos || []}
             keyExtractor={(item) => item.id}
-            itemsByRow={4}
+            itemsByRow={columnCount}
             rowClassName="p2"
             cellClassName="p-4 flex flex-1 w-32"
             renderItem={CardVideo}
